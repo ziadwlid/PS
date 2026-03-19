6c88bb3290914644c1da83ddc4a23cc60b3a249f
Ziad Walid <ziyadskillup@gmail.com>
2026-03-19 13:17:03 +0200

    I_Divisability -> another sol to reduce time limit exceeded

--- Codeforces/assiut_unv/sheet6_math-geometry/I_Divisability.cpp
+++ Codeforces/assiut_unv/sheet6_math-geometry/I_Divisability.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-
+#include <algorithm>
+long long sum (long long n){
+    return (n * (n + 1) / 2);
+}
 int main (){
     long long a {0}, b {0}, x {0};
     std::cin >> a >> b >> x;
-    long long sum {0};
-    for (int i = a; i <= b; i++){
-        if (i % x == 0) sum += i;
-    }
-    std::cout << sum << "\n";
+    long long ma {0}, mi {0};
+    ma = std::max (a, b);
+    mi = std::min (a, b);
+    std::cout << sum(ma/x) * x - sum((mi -1) / x) * x;
     return 0;
 }
