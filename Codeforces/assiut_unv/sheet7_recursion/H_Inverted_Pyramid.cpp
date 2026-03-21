f220d8a3dbd5044d7758c6e857e47adbf7705f62
Ziad Walid <ziyadskillup@gmail.com>
2026-03-21 22:32:58 +0200

    H_Inverted_Pyramid

---
+++ Codeforces/assiut_unv/sheet7_recursion/H_Inverted_Pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+void print_space (int space){
+    if (space == 0) return;
+    std::cout << " ";
+    print_space(space - 1);
+}
+void print_astetisk (int asterisk){
+    if (asterisk == 0) return;
+    std::cout << "*";
+    print_astetisk(asterisk - 1);
+}
+void pyramid(int n, int nn){
+    if (n == 0) return;
+    pyramid(n - 1, nn);
+    print_space(n - 1);
+    print_astetisk(2 * (nn - n + 1) - 1);  
+    std::cout << "\n";
+}
+int main (){
+    int n {0};
+    std::cin >> n;
+    pyramid(n, n);
+    return 0;
+}
