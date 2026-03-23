10d166a655e014a296be7171930773083e77a611
Ziad Walid <ziyadskillup@gmail.com>
2026-03-23 21:17:16 +0200

    A_Palindromes_Replace

--- Codeforces/assiut_unv/sheet8_general-easy/A_Palindromes_Replace.cpp
+++ Codeforces/assiut_unv/sheet8_general-easy/A_Palindromes_Replace.cpp
@@ -2,6 +2,22 @@
 
 int main (){
     std::string str; std::cin >> str;
-    
+    int i = 0, j = str.length()-1;
+    bool impossible = false;
+    while (i <= j) {
+        if (str[i] == '?' && str[j] == '?') {
+            str[i] = str[j] = 'a';       
+        } else if (str[i] == '?') {
+            str[i] = str[j];
+        } else if (str[j] == '?') {
+            str[j] = str[i];
+        } else if (str[i] != str[j]) {
+            impossible = true;             
+            break;
+        }
+        i++; j--;
+    }
+    if (impossible) std::cout << "-1\n";
+    else std::cout << str << "\n";
     return 0;
 }
