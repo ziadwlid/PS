d50233741ae2affa27de546a5eafd09bc1581c36
Ziad Walid <ziyadskillup@gmail.com>
2026-03-23 22:40:10 +0200

    H_Presents

--- Codeforces/assiut_unv/sheet8_general-easy/H_Presents.cpp
+++ Codeforces/assiut_unv/sheet8_general-easy/H_Presents.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main (){
-    
+    int n_friends {0}; std::cin >> n_friends;
+    int arr[n_friends], result[n_friends];
+    for (int i = 0; i < n_friends; i++){
+        std::cin >> arr[i];
+    }
+    for (int i = 0; i < n_friends; i++){
+        result[arr[i] - 1] = i + 1;
+    }
+    for (int i = 0; i < n_friends; i++){
+        std::cout << result[i] << " ";
+    }
     return 0;
 }
