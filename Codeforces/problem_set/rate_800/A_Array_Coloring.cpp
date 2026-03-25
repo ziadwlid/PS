71018a590361afdb2bf6521991a04b3de361b4f8
Ziad Walid <ziyadskillup@gmail.com>
2026-03-25 13:31:28 +0200

    A_Array_Coloring

---
+++ Codeforces/problem_set/rate_800/A_Array_Coloring.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int t{0}; std::cin >> t;
+    int length {0};
+    int arr [length];
+    while (t--){
+        std::cin >> length;
+        for (int i = 0; i < length; i++){
+            std::cin >> arr[i];
+        }
+        
+
+    }
+    return 0;
+}
