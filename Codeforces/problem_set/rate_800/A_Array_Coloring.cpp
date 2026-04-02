0726bfd31ecf381b205aa41e08d0a2f378f54199
Ziad Walid <ziyadskillup@gmail.com>
2026-04-02 19:58:38 +0200

    A_Array_Coloring

--- Codeforces/problem_set/rate_800/A_Array_Coloring.cpp
+++ Codeforces/problem_set/rate_800/A_Array_Coloring.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-
-int main(){
-    int t{0}; std::cin >> t;
-    int length {0};
-    int arr [length];
-    while (t--){
+#include <vector>
+int main() {
+    int t {0};
+    std::cin >> t;
+    while (t--) {
+        int length {0};
         std::cin >> length;
-        for (int i = 0; i < length; i++){
-            std::cin >> arr[i];
+        std::vector<int> arr(length);
+        for (auto &i : arr) std::cin >> i;
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            count += (arr[i] % 2 != (i+1) % 2);
         }
-        
-
+        std::cout << (count == 0 || count == length ? "YES" : "NO") << "\n";
     }
     return 0;
 }
