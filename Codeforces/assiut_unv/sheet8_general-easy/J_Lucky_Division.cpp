8299665375dfcd893be0de5a84d528ef6896ab04
Ziad Walid <ziyadskillup@gmail.com>
2026-04-02 21:30:15 +0200

    J_Lucky_Division

--- Codeforces/assiut_unv/sheet8_general-easy/J_Lucky_Division.cpp
+++ Codeforces/assiut_unv/sheet8_general-easy/J_Lucky_Division.cpp
@@ -4,16 +4,15 @@ int main()
 {
     int num{0};
     std::cin >> num;
+
+    int original = num; 
     int rest{0};
     bool flag = false;
+
     while (true)
     {
         rest = num % 10;
-        if (num % 4 == 0)
-        {
-            flag = true;
-            break;
-        }
+
         if (rest == 4 || rest == 7)
         {
             flag = true;
@@ -23,10 +22,27 @@ int main()
         }
         else
         {
-            std::cout << "NO\n";
+            flag = false;
             break;
         }
     }
+
+    if (!flag)
+    {
+        int lucky[] = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+
+        for (int i = 0; i < 14; i++)
+        {
+            if (original % lucky[i] == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+    }
+
     if (flag) std::cout << "YES\n";
+    else std::cout << "NO\n";
+
     return 0;
 }
