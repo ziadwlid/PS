5467af6c60c262a4a31d1452ce24fd405d238958
Ziad Walid <ziyadskillup@gmail.com>
2026-04-22 19:43:08 +0200

    E_Difference.cpp

---
+++ platforms/Codeforces/mansoura_unv/level0_2025/sheet1/E_Difference.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main (){
+    long long a {}, b {}, c {}, d{};
+    std::cin >> a >> b >> c >> d;
+    std::cout << "DIFERENCA = " << ((a*b) - (c*d)) << "\n";
+    return 0;
+}
