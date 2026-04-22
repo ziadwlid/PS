7382fb087ff3465aad9839c10bbbf46955d745e9
Ziad Walid <ziyadskillup@gmail.com>
2026-04-22 18:57:54 +0200

    changes

---
+++ platforms/Codeforces/mansoura_unv/level0_2025/sheet1/V_Sum_of_Odd_Integers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+bool sum_of_odd_nums(long long n, long long k){
+    return (n >= k && (n - k) % 2 == 0);
+}
+
+int main (){
+    int test_cases{};
+    std::cin >> test_cases;
+    long long n {}, k {};
+    while (test_cases > 0){
+        std::cin >> n >> k;
+        if (!sum_of_odd_nums(n, k)) std::cout << "NO\n";
+        else std::cout << "YES\n";
+        --test_cases;
+    }
+    
+    return 0;
+}
