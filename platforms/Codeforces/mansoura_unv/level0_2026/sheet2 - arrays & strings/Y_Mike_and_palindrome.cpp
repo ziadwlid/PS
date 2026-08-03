f9729ec026662dfd2375c151024782ad3a0dbbaa
Ziad Walid <ziyadskillup@gmail.com>
2026-08-03 15:11:30 +0300

    Y_Mike_and_palindrome

---
+++ platforms/Codeforces/mansoura_unv/level0_2026/sheet2 - arrays & strings/Y_Mike_and_palindrome.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+int main()
+{
+    std::string str;
+    std::cin >> str;
+    int diff {};
+    for (int i = 0, j = str.length() - 1; i < j; i++, j--){
+        if (str[i] != str[j]) 
+            diff++;
+    }
+    if (str.size() % 2 == 1 && diff < 0) diff++;
+    if (diff == 1) std::cout << "YES\n";
+    else std::cout << "NO\n"; 
+    return 0;
+}
