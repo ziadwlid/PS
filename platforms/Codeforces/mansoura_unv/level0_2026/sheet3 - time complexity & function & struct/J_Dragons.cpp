2bfa775d1e388e9d326a17e1ea16f2763f4151fb
Ziad Walid <ziyadskillup@gmail.com>
2026-08-09 23:47:51 +0300

    J_Dragons

--- platforms/Codeforces/mansoura_unv/level0_2026/sheet3 - time complexity & function & struct/J_Dragons.cpp
+++ platforms/Codeforces/mansoura_unv/level0_2026/sheet3 - time complexity & function & struct/J_Dragons.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>
+struct Dragon{
+    int strength;
+    int bonus;
+};
+bool cmp (Dragon a, Dragon b){
+    return a.strength < b.strength;
+}
 int main()
 {
-    int s, n;
-    std::cin >> s >> n;
-    
+    int kirito_strength, n_dragons;
+    std::cin >> kirito_strength >> n_dragons;
+    Dragon dragons[n_dragons];
+    for (int i = 0; i < n_dragons; i++){
+        std::cin >> dragons[i].strength >> dragons[i].bonus;
+    }
+
+    std::sort (dragons, dragons+n_dragons, cmp);
+    for (const auto& dragon : dragons)
+    {
+        if (kirito_strength <= dragon.strength)
+        {
+            std::cout << "NO\n";
+            return 0;
+        }
+
+        kirito_strength += dragon.bonus;
+    }
+
+    std::cout << "YES\n";
+
     return 0;
 }
