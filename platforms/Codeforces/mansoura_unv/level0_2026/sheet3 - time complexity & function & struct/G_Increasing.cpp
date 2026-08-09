2917cd8403a1a675552bba20606d82db1cde95d8
Ziad Walid <ziyadskillup@gmail.com>
2026-08-09 12:52:01 +0300

    G_Increasing

--- platforms/Codeforces/mansoura_unv/level0_2026/sheet3 - time complexity & function & struct/G_Increasing.cpp
+++ platforms/Codeforces/mansoura_unv/level0_2026/sheet3 - time complexity & function & struct/G_Increasing.cpp
@@ -19,8 +19,10 @@ int main()
         for (int i = 1; i < n; i++)
         {
             if (arr[i] <= arr[i - 1])
+            {
                 flag = false;
-            break;
+                break;
+            }
         }
         std::cout << (flag ? "YES\n" : "NO\n");
     }
