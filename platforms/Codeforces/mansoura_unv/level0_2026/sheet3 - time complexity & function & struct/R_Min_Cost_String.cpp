0710a82e4115272560823896ce10e36fade2e75e
Ziad Walid <ziyadskillup@gmail.com>
2026-08-16 19:56:16 +0300

    R_Min_Cost_String not complete yet

---
+++ platforms/Codeforces/mansoura_unv/level0_2026/sheet3 - time complexity & function & struct/R_Min_Cost_String.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    std::string str;
+    int arr[27];
+    std::cin >> str;
+    for (int i = 0; i < 27; i++){
+        std::cin >> arr[i];
+    }
+    int cost {};
+    std::string output;
+    for (int i = 0; i < str.length(); i++){
+        if (str[i] == '?') {
+            
+        } 
+    }
+    return 0;
+}
