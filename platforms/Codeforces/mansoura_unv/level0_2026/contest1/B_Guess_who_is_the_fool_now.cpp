8677c2e072503df71fd0cdb84bef3601034ed53a
Ziad Walid <ziyadskillup@gmail.com>
2026-08-17 23:05:44 +0300

    B_Guess_who_is_the_fool_now

---
+++ platforms/Codeforces/mansoura_unv/level0_2026/contest1/B_Guess_who_is_the_fool_now.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int n {};
+    std::cin >> n;
+    while (n--){
+        std::cout << "This problem is as useless as adham el saady\n";
+    }
+    return 0;
+}
